b46ec30b168fae98ab84754196e2cd91af5dd5fb
israel.paixao <israeldouglas25@gmail.com>
2025-07-27 17:00:19 -0300

    logica do microservice de delivery

---
+++ Microservice/Delivery-Tracking/src/main/java/com/algaworks/algadelivery/delivery/tracking/domain/service/DeliveryPreparationService.java
@@ -0,0 +1,86 @@
+package com.algaworks.algadelivery.delivery.tracking.domain.service;
+
+import com.algaworks.algadelivery.delivery.tracking.api.model.ContactPointInput;
+import com.algaworks.algadelivery.delivery.tracking.api.model.DeliveryInput;
+import com.algaworks.algadelivery.delivery.tracking.api.model.ItemInput;
+import com.algaworks.algadelivery.delivery.tracking.domain.exception.DomainException;
+import com.algaworks.algadelivery.delivery.tracking.domain.model.ContactPoint;
+import com.algaworks.algadelivery.delivery.tracking.domain.model.Delivery;
+import com.algaworks.algadelivery.delivery.tracking.domain.repository.DeliveryRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.math.BigDecimal;
+import java.time.Duration;
+import java.util.UUID;
+
+@Service
+@RequiredArgsConstructor
+public class DeliveryPreparationService {
+
+    private final DeliveryRepository deliveryRepository;
+
+    @Transactional
+    public Delivery draft(DeliveryInput input) {
+        Delivery delivery = Delivery.draft();
+        handlePreparation(input, delivery);
+
+        return deliveryRepository.saveAndFlush(delivery);
+    }
+
+    @Transactional
+    public Delivery edit(UUID deliveryId, DeliveryInput input) {
+        Delivery delivery = deliveryRepository.findById(deliveryId)
+                .orElseThrow(() -> new DomainException("Delivery not found"));
+        delivery.removeItems();
+        handlePreparation(input, delivery);
+
+        return deliveryRepository.saveAndFlush(delivery);
+    }
+
+    private void handlePreparation(DeliveryInput input, Delivery delivery) {
+
+        ContactPointInput senderInput = input.getSender();
+        ContactPointInput recipientInput = input.getRecipient();
+
+        ContactPoint sender = ContactPoint.builder()
+                .zipCode(senderInput.getZipCode())
+                .street(senderInput.getStreet())
+                .number(senderInput.getNumber())
+                .complement(senderInput.getComplement())
+                .name(senderInput.getName())
+                .phone(senderInput.getPhone())
+                .build();
+
+        ContactPoint recipient = ContactPoint.builder()
+                .zipCode(recipientInput.getZipCode())
+                .street(recipientInput.getStreet())
+                .number(recipientInput.getNumber())
+                .complement(recipientInput.getComplement())
+                .name(recipientInput.getName())
+                .phone(recipientInput.getPhone())
+                .build();
+
+        Duration expectedDeliveryTime = Duration.ofHours(3);
+        BigDecimal distanceFee = BigDecimal.valueOf(10.00);
+
+        BigDecimal payout = BigDecimal.valueOf(10.00);
+
+        Delivery.PreparationDetails preparationDetails = Delivery.PreparationDetails.builder()
+                .sender(sender)
+                .recipient(recipient)
+                .expectedDeliveryTime(expectedDeliveryTime)
+                .courierPayout(payout)
+                .distanceFee(distanceFee)
+                .build();
+
+        delivery.editPreparationDetails(preparationDetails);
+
+        for (ItemInput itemInput : input.getItems()) {
+            delivery.addItem(itemInput.getName(), itemInput.getQuantity());
+        }
+    }
+}
+
+
